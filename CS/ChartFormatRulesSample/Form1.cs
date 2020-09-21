a3223dbb3311cbde85ff0e3be55f463fdbfe6fbd
Margarita Zakhodyaeva (DevExpress) <zakhodyaeva@corp.devexpress.com>
2020-09-21 19:21:10 +0300

    change CS solution

--- CS/ChartFormatRulesSample/Form1.cs
+++ CS/ChartFormatRulesSample/Form1.cs
@@ -11,21 +11,21 @@ namespace ScatterChartFormatRulesSample
             dashboardDesigner1.CreateRibbon();
             dashboardDesigner1.LoadDashboard(@"../../nwindDashboard.xml");
             ScatterChartDashboardItem scatterChart1 = (ScatterChartDashboardItem)dashboardDesigner1.Dashboard.Items["scatterChartDashboardItem1"];
-            AddFormatRulesToBarSeries(scatterChart1);
+            AddFormatRulesToScatterChart(scatterChart1);
             new ScatterChartConstantLineUserValueModule(dashboardDesigner1, svgImageCollection1["chartrangearea"]);
         }
-        public void AddFormatRulesToBarSeries(ScatterChartDashboardItem scatterChart) {
+        public void AddFormatRulesToScatterChart(ScatterChartDashboardItem scatterChart) {
             double yValue = 7000;
             double xValue = 18;
-            ScatterChartItemFormatRule valueRule1 = new ScatterChartItemFormatRule();
-            valueRule1.DataItem = scatterChart.AxisYMeasure;
+            ScatterChartItemFormatRule expressionRule1 = new ScatterChartItemFormatRule();
+            expressionRule1.DataItem = scatterChart.AxisYMeasure;
             FormatConditionExpression formatCondition = new FormatConditionExpression();
             formatCondition.Expression = $"{scatterChart.AxisYMeasure.UniqueId} > {yValue} && {scatterChart.AxisXMeasure.UniqueId} > {xValue}";
             formatCondition.StyleSettings = new ColorStyleSettings(ColorTranslator.FromHtml("#14abb7"));
-            valueRule1.Condition = formatCondition;
-            valueRule1.ShowInLegend = true;
-            valueRule1.DisplayName = "Discount amount from the quantity of products sold";
-            scatterChart.FormatRules.Add(valueRule1);
+            expressionRule1.Condition = formatCondition;
+            expressionRule1.ShowInLegend = true;
+            expressionRule1.DisplayName = "Discount amount from the quantity of products sold";
+            scatterChart.FormatRules.Add(expressionRule1);
 
             ScatterChartConstantLineUserData moduleData = new ScatterChartConstantLineUserData() { Enabled = true, VerticalAxisValue = xValue, HorizontalAxisValue = yValue };
             scatterChart.CustomProperties.SetValue(ScatterChartConstantLineUserValueModule.PropertyName, moduleData.GetStringFromData());
